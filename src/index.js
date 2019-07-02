c9043b72d6c13b478435f1a149eb3c5591596bc5
brodave318 <brodave318@gmail.com>
2019-07-02 14:09:37 -0700

    Add Tooltip functionality & css

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-import './test.css';
+import Tooltip from './ninjr-ui/tooltip';
 
-console.log('index.js file');
+// create a tooltip
+const tooltip = new Tooltip(document.querySelector('.tooltip'));
+
+tooltip.init();
